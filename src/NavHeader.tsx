bd1cf1fc4097746bacbf5bb425f81919889d92e5
TheDanishMexican <danieljchristjansen@gmail.com>
2024-03-01 17:43:12 +0100

    made it to QueryParams and useSearchParams

--- src/NavHeader.tsx
+++ src/NavHeader.tsx
@@ -1,28 +1,27 @@
-import { Link } from "react-router-dom";
-import AuthStatus from "./security/AuthStatus";
+import { NavLink } from 'react-router-dom'
+import AuthStatus from './security/AuthStatus'
 
 export default function NavHeader() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-          {/* <Link to="/">Home</Link> */}
-        </li>
-        <li>
-          <a href="/categories">Categories</a>
-          {/* <Link to="/categories">Categories</Link> */}
-        </li>
-        <li>
-          <a href="/recipes">Recipes</a>
-          {/* <Link to="/recipes">Recipes</Link> */}
-        </li>
-        <li>
-          <a href="/add">Add</a>
-          {/* <Link to="/add">Add</Link> */}
-        </li>
-        <AuthStatus />
-      </ul>
-    </nav>
-  );
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/categories">Categories</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/recipes">Recipes</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/add">Add</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact">Contact</NavLink>
+                </li>
+                <AuthStatus />
+            </ul>
+        </nav>
+    )
 }
